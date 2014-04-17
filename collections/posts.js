7e1f4ca01584dcd5fff366bce0a668f095ea5ad7
eluck <mail@eluck.me>
2014-04-18 00:15:56 +0700

    Refactor 'post' method to unittestable state

--- collections/posts.js
+++ collections/posts.js
@@ -13,38 +13,7 @@ Posts.deny({
 });
 
 Meteor.methods({
-  post: function(postAttributes) {
-    var user = Meteor.user(),
-      postWithSameLink = Posts.findOne({url: postAttributes.url});
-    
-    // ensure the user is logged in
-    if (!user)
-      throw new Meteor.Error(401, "You need to login to post new stories");
-    
-    // ensure the post has a title
-    if (!postAttributes.title)
-      throw new Meteor.Error(422, 'Please fill in a headline');
-    
-    // check that there are no previous posts with the same link
-    if (postAttributes.url && postWithSameLink) {
-      throw new Meteor.Error(302, 
-        'This link has already been posted', 
-        postWithSameLink._id);
-    }
-    
-    // pick out the whitelisted keys
-    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
-      userId: user._id, 
-      author: user.username, 
-      submitted: new Date().getTime(),
-      commentsCount: 0,
-      upvoters: [], votes: 0
-    });
-    
-    var postId = Posts.insert(post);
-    
-    return postId;
-  },
+  post: PostsHelper.insertNewPost,
   
   upvote: function(postId) {
     var user = Meteor.user();
